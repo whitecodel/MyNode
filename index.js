b1e1f29ad51fbaf00101ea52cf52ad5dc9d9ee42
whitecodel <henryroy40@gmail.com>
2022-05-06 11:32:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  console.log(element);
+}
+
+console.log(index);
