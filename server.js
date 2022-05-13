e6f81f70820abfa6170381abef4c6491a30f6119
whitecodel <henryroy40@gmail.com>
2022-05-13 07:52:13 +0530

    http server complected

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const http = require("http");
 const headers = { "Content-Type": "text/html" };
 
 function server(req, res) {
-  res.writeHead(500, headers);
-  res.end("Hello World! I am a server!");
+  const url = req.url;
+
+  switch (url) {
+    case "/":
+      res.writeHead(200, headers);
+      res.end("home page");
+      break;
+    case "/about":
+      res.writeHead(200, headers);
+      res.end("about page");
+    default:
+      res.writeHead(404, headers);
+      res.end("Not Found");
+  }
 }
 
 http.createServer(server).listen(8080);
